3773f0b05dd1f2d8ad590321d9f2b0a0fd386435
Nemanja <kn@opinodo.com>
2019-04-10 11:00:31 +0200

    Napravljene klase za korisnike

---
+++ car_rental_project/Administrator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car_rental_project
+{
+    class Administrator : Korisnik
+    {
+        public Administrator(string korisnickoIme, string lozinka) : base(korisnickoIme, lozinka)
+        {
+            
+        }
+
+        
+    }
+}
---
+++ car_rental_project/Korisnik.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car_rental_project
+{
+    abstract class Korisnik
+    {
+        private string korisnickoIme;
+        private string lozinka;
+
+
+        protected Korisnik(string korisnickoIme, string lozinka)
+        {
+            this.korisnickoIme = korisnickoIme;
+            this.lozinka = lozinka;
+        }
+
+        protected string KorisnickoIme { get => korisnickoIme; set => korisnickoIme = value; }
+        protected string Lozinka { get => lozinka; set => lozinka = value; }
+
+    }
+}
